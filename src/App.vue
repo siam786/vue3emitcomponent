5f311b0886cff1d2ae4d7571077a89cb4f7271fa
siam786e <shohrab.hossain36@gmail.com>
2023-08-20 12:35:14 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from './assets/components/Header.vue';
+import Footer from './assets/components/Footer.vue';
+import ParentComponent from './assets/components/ParentComponent.vue';
+</script>
+
+<template >
+  <Header/>
+  <div class="flex items-center justify-center h-screen bg-gray-900">
+    <ParentComponent/>
+  </div>
+<Footer/>
+</template>
+
